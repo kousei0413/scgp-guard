7cee128220edb7ab450f0d5c287ead72f76f1f99
kousei0413 <kousei085875900@outlook.jp>
2026-06-11 20:03:23 +0900

    Update page.tsx

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -8,17 +8,13 @@ export default function Home() {
       
       {/* タイポグラフィ（組織概要・お知らせの文字データのみに集約） */}
       <div className="space-y-6">
-        <div className="text-xs font-bold tracking-widest text-blue-600 uppercase">
-         
-        </div>
+        
         
         <h1 className="text-4xl sm:text-6xl font-black text-red-550 tracking-tight leading-tight">
           こちらはテストページです、引き返してください<br />
         </h1>
         
-        <p className="text-gray-500 text-sm sm:text-base max-w-xl leading-relaxed font-medium">
-          
-        </p>
+        
         
         <p className="text-gray-500 text-sm sm:text-base max-w-xl leading-relaxed font-medium">
           
